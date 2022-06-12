a86575a6f55ba142b39276a0ff4236cce8457bb9
moises gomez <moisesg526@yahoo.com>
2022-06-12 00:49:25 -0400

    Updates

---
+++ precache-manifest.c506d7fe113df88b9a8c18e79583fa09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142d405af26455feb9019da8e4fff1f7",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "bf1ee83b451e2fccf3ba",
+    "url": "/recipe-app/static/css/main.c1cc3a9f.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/recipe-app/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-app/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1ee83b451e2fccf3ba",
+    "url": "/recipe-app/static/js/main.51f82edd.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
